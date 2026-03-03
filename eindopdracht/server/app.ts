2ad17d6d41667edd3aa2aac47d2fea8c4b5481ce
Finn Van Eyck <finn.vaneyck@student.arteveldehs.be>
2026-03-03 20:13:54 +0100

    mocking data gemaakt en app de JSON laten verwerken

--- eindopdracht/server/app.ts
+++ eindopdracht/server/app.ts
@@ -3,6 +3,7 @@ import express, { Application, Request, Response } from "express";
 const app: Application = express();
 const PORT: number = 3000;
 
+app.use(express.json());
 app.get("/", (req: Request, res: Response): void => {
   res.send("Hallo, dit is je eerste Express-server met TypeScript!");
 });
